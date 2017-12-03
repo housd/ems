799eaad21aab80c8a89c07da9ffac14aad21690a
housd <hsd19930705@163.com>
2017-12-03 15:38:02 +0800

    新的ssdfdg

--- emssss/src/main/java/com/baizhi/asd.java
+++ emssss/src/main/java/com/baizhi/asd.java
@@ -4,5 +4,6 @@ public class asd {
     public static void main(String[] args) {
         System.out.println("hasd");
         System.out.println("jasd");
+        System.out.println("ASdsad");
     }
 }
